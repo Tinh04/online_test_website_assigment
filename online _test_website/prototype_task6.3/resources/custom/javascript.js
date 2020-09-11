2d7f3dfa3fc033f5ebbb5bae275eac4017c06fdc
Tinh04 <mytinh04@gmail.com>
2020-09-11 15:10:35 +0700

    online_test_website

---
+++ online _test_website/prototype_task6.3/resources/custom/javascript.js
@@ -0,0 +1,17 @@
+function submitForm() {
+    window.location.href = './information.html';
+    return false;
+}
+
+let check = function() {
+    if (document.getElementById('password').value ==
+        document.getElementById('confirm_password').value) {
+        document.getElementById('message').style.color = 'green';
+        document.getElementById('message').innerHTML = 'matching';
+        document.getElementById('submit').disabled = false;
+    } else {
+        document.getElementById('message').style.color = 'red';
+        document.getElementById('message').innerHTML = 'not matching';
+        document.getElementById('submit').disabled = true;
+    }
+}
